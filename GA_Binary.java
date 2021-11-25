b68897af1b3e6143af79a8f7f6d6b5c9383257db
Visethboti <visethboti@gmail.com>
2021-11-25 14:28:32 -0600

    Implemented crossover and elitism

--- GA_Binary.java
+++ GA_Binary.java
@@ -15,16 +15,17 @@ public class GA_Binary {
 	
 	// Parameters
 	private final int popsSize = 10;
-	private final double parentSizePercentage = 0.5; 
-	private final double offspringSizePercentage = 0.98;
-	private final double elitismSizePercentage = 0.02;
+	private final double parentSizePercentage = 0.2; 
+	private final double offspringSizePercentage = 0.1;
+	private final double elitismSizePercentage = 0.2;
 	private final double mutationProbality = 0.0005;
 	private final int kTournamentSize = 3;
 	
 	// Parameters Size
 	private final int parentSize = (int) (popsSize * parentSizePercentage);
 	private final int offspringSize = (int) (popsSize * offspringSizePercentage);
-	private final int elitismSize = popsSize - offspringSize;
+	//private final int elitismSize = popsSize - offspringSize;
+	private final int elitismSize = (int) (popsSize * elitismSizePercentage);
 	
 	// Initialization
 	private char[][] populations; // populations[][0][] is Value // populations[][1][] is Weight
@@ -69,6 +70,12 @@ public class GA_Binary {
 		fitnessCalculation();
 		printPopulations();
 		parentSelection();
+		crossOver();
+		elitism();
+		
+		printParentPool();
+		printOffspringPool();
+		printElitismPool();
 	}
 	
 	// EA Methods
@@ -95,6 +102,11 @@ public class GA_Binary {
 	}
 	
 	private void parentSelection(){ // K-tournament Selection
+		// clear Parent Pool
+		for(int i = 0; i < parentSize; i++) {
+			parentPool[i] = -1;
+		}
+		
 		//int[] tournament = new int[kTournamentSize]; // to store the index to the chromosome randomly choosen from population
 		int bestChromosomeIndex;
 		int randomNum;
@@ -112,16 +124,66 @@ public class GA_Binary {
 			}
 			
 			parentPool[i] = bestChromosomeIndex;
-			System.out.println("parent: "+ bestChromosomeIndex);
+			//System.out.println("parent: "+ bestChromosomeIndex); // for debugging
 		}
 	}
 	
 	private void crossOver(){
-		
+		int randomNum1, randomNum2, randomPoint;
+		for(int i = 0; i < offspringSize; i++) { // For each num of off spring required
+			randomNum1 = random.nextInt(parentSize); // Randomly pick two parent from the parentPool that is not the same
+			do {
+				randomNum2 = random.nextInt(parentSize);
+			}while(randomNum1 == randomNum2);
+			
+			// One point crossover
+			randomPoint = random.nextInt(qkNumObjects-1) + 1; // randomPoint between 1 to number of objects
+			
+			for(int j = 0; j < randomPoint; j++) {
+				offspringPool[i][j] = populations[parentPool[randomNum1]][j];
+			}
+			for(int j = randomPoint; j < qkNumObjects; j++) {
+				offspringPool[i][j] = populations[parentPool[randomNum2]][j];
+			}
+		}
 	}
 	
 	private void elitism() {
+		int[] elitismIndex = new int[elitismSize];
+		for(int i = 0; i < elitismSize; i++) 
+			elitismIndex[i] = -1;
 		
+		int indexHighestFitness = 0;
+		int highestFitness = 0;
+		boolean notExist = true;
+		
+		// get all the indexs
+		for(int i = 0; i < elitismSize; i++){	
+			for(int j = 0; j < popsSize; j++){
+				if(!notExistInArray(elitismIndex, j)){
+					notExist = false;
+				}
+				
+				if(highestFitness < popFitness[j] && notExist){
+					highestFitness = popFitness[j];
+					indexHighestFitness = j;
+				}
+				notExist = true;
+			}
+			elitismIndex[i] = indexHighestFitness;
+			indexHighestFitness = 0;
+			highestFitness = 0;	
+		}
+		
+		// put the index pop into elitismPool
+		for(int i = 0; i < elitismSize; i++){
+			for(int j = 0; j < qkNumObjects; j++){
+					elitismPool[i][j] = populations[elitismIndex[i]][j];
+			}
+			//System.out.println("Elitism - " + elitismIndex[i]);// for debugging
+		}
+		
+		return;
 	}
 	
 	private void replaceGeneration(){
@@ -155,12 +217,41 @@ public class GA_Binary {
 			return totalValue;
 	}
 	
-	// Untilities Methods
+	// *** Untilities Methods ***
+	
+	// Print Methods
 	private void printPopulations(){
 		for(int i = 0; i < popsSize; i++) {
 			System.out.print(i+"-");
 			System.out.print(populations[i]);
 			System.out.println(" Fitness: " + popFitness[i]);
+		}
+	}
+	
+	private void printParentPool(){
+		System.out.println("*** Parent Pool ***");
+		for(int i = 0; i < parentSize; i++) {
+			System.out.print(i+"-Pop:" + parentPool[i] + "-");
+			System.out.print(populations[parentPool[i]]);
+			System.out.println(" Fitness: " + popFitness[parentPool[i]]);
+		}
+	}
+	
+	private void printOffspringPool(){
+		System.out.println("*** Offspring Pool ***");
+		for(int i = 0; i < offspringSize; i++) {
+			System.out.print(i+"-");
+			System.out.print(offspringPool[i]);
+			System.out.println(" Fitness: " + getFitness(offspringPool[i]));
+		}
+	}
+	
+	private void printElitismPool(){
+		System.out.println("*** Elitism Pool ***");
+		for(int i = 0; i < elitismSize; i++) {
+			System.out.print(i+"-");
+			System.out.print(elitismPool[i]);
+			System.out.println(" Fitness: " + getFitness(elitismPool[i]));
 		}
 	}
 	
@@ -171,7 +262,6 @@ public class GA_Binary {
 		}
 		return true;
 	}
-	
 }
 
 
