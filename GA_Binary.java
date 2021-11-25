c660f9b5a4734ae53dfc40330a3dacbdb6b8fa7b
Visethboti <visethboti@gmail.com>
2021-11-25 15:04:45 -0600

    implemented Elitism, ReplaceGeneration and Mutation (Mutation with contraint where only fesible mutating is allowed)

--- GA_Binary.java
+++ GA_Binary.java
@@ -15,10 +15,10 @@ public class GA_Binary {
 	
 	// Parameters
 	private final int popsSize = 10;
-	private final double parentSizePercentage = 0.2; 
-	private final double offspringSizePercentage = 0.1;
-	private final double elitismSizePercentage = 0.2;
-	private final double mutationProbality = 0.0005;
+	private final double parentSizePercentage = 0.5; 
+	private final double offspringSizePercentage = 0.98;
+	private final double elitismSizePercentage = 0.02;
+	private final double mutationProbality = 1;
 	private final int kTournamentSize = 3;
 	
 	// Parameters Size
@@ -76,6 +76,14 @@ public class GA_Binary {
 		printParentPool();
 		printOffspringPool();
 		printElitismPool();
+		
+		replaceGeneration();
+		fitnessCalculation();
+		printPopulations();
+		
+		mutation();
+		fitnessCalculation();
+		printPopulations();
 	}
 	
 	// EA Methods
@@ -130,20 +138,28 @@ public class GA_Binary {
 	
 	private void crossOver(){
 		int randomNum1, randomNum2, randomPoint;
+		char[] newOffspring = new char[qkNumObjects];
 		for(int i = 0; i < offspringSize; i++) { // For each num of off spring required
-			randomNum1 = random.nextInt(parentSize); // Randomly pick two parent from the parentPool that is not the same
 			do {
-				randomNum2 = random.nextInt(parentSize);
-			}while(randomNum1 == randomNum2);
+				randomNum1 = random.nextInt(parentSize); // Randomly pick two parent from the parentPool that is not the same
+				do {
+					randomNum2 = random.nextInt(parentSize);
+				}while(randomNum1 == randomNum2);
+				
+				// One point crossover
+				randomPoint = random.nextInt(qkNumObjects-1) + 1; // randomPoint between 1 to number of objects
+				
+				for(int j = 0; j < randomPoint; j++) {
+					newOffspring[j] = populations[parentPool[randomNum1]][j];
+				}
+				for(int j = randomPoint; j < qkNumObjects; j++) {
+					newOffspring[j] = populations[parentPool[randomNum2]][j];
+				}
+			}while(getFitness(newOffspring) == 0);
 			
-			// One point crossover
-			randomPoint = random.nextInt(qkNumObjects-1) + 1; // randomPoint between 1 to number of objects
-			
-			for(int j = 0; j < randomPoint; j++) {
-				offspringPool[i][j] = populations[parentPool[randomNum1]][j];
-			}
-			for(int j = randomPoint; j < qkNumObjects; j++) {
-				offspringPool[i][j] = populations[parentPool[randomNum2]][j];
+			// Add to offspring Pool
+			for(int j = 0; j < qkNumObjects; j++) {
+				offspringPool[i][j] = newOffspring[j];
 			}
 		}
 	}
@@ -187,11 +203,59 @@ public class GA_Binary {
 	}
 	
 	private void replaceGeneration(){
+		int counter = 0;
 		
+		// Combine offspring and elitismPool to make the next generation
+		for(int i = 0; i < offspringSize && counter < popsSize; i++){
+			for(int j = 0; j < qkNumObjects; j++){
+				populations[counter][j] = offspringPool[i][j];
+			}
+			counter++;
+		}
+		
+		for(int i = 0; i < elitismSize && counter < popsSize; i++){
+			for(int j = 0; j < qkNumObjects; j++){
+				populations[counter][j] = elitismPool[i][j];
+			}
+			counter++;
+		}
+		
+		return;
 	}
 	
 	private void mutation(){
-		
+		double randomValue;
+		int randomBit;
+		int counter, currentCFitness;
+		char[] currentChromosome = new char[qkNumObjects];
+		for(int i = 0; i < popsSize; i++) {
+			randomValue = random.nextDouble();
+			if(randomValue <= mutationProbality) {
+				// take current chromosome from population
+				for(int j = 0; j < qkNumObjects; j++) {
+					currentChromosome[j] = populations[i][j];
+				}
+				
+				counter = 0;
+				currentCFitness = 0;
+				do {
+					randomBit = random.nextInt(qkNumObjects);
+					if(currentChromosome[randomBit] == '1')
+						currentChromosome[randomBit] = '0';
+					else
+						currentChromosome[randomBit] = '1';
+					counter++;
+					currentCFitness = getFitness(currentChromosome);
+				}while(counter < qkNumObjects && currentCFitness == 0);
+					
+				// Replace the pop with currentChromosome
+				if(currentCFitness > 0) {
+					for(int j = 0; j < qkNumObjects; j++) {
+						populations[i][j] = currentChromosome[j];
+					}
+				}
+			}
+		}
 	}
 	
 	// GA Untilities Methods
@@ -221,6 +285,7 @@ public class GA_Binary {
 	
 	// Print Methods
 	private void printPopulations(){
+		System.out.println("*** Populations ***");
 		for(int i = 0; i < popsSize; i++) {
 			System.out.print(i+"-");
 			System.out.print(populations[i]);
